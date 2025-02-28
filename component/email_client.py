c196308ecba7a9d0914b70ecb7dd1328c23686d6
许博 <xubo@hopechart.com>
2025-02-28 16:31:38 +0800

    修改邮件标题

--- component/email_client.py
+++ component/email_client.py
@@ -5,14 +5,17 @@ from email import encoders
 from email.MIMEText import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
-from email.utils import  formatdate
+from email.utils import formatdate
 
 from common import common as cm
-
+from config.config import Config
 
 
 class EmailClient:
+    config = Config()
+
     def send_zip_via_email(self, zip_path):
+        project = self.config.get_project()
         smtp_server = 'smtp.exmail.qq.com'
         smtp_port = 465
         username = 'hqerp@hopechart.com'
@@ -20,15 +23,14 @@ class EmailClient:
         from_addr = 'hqerp@hopechart.com'
         to_addrs = 'bo.xu@hopechart.com'
 
-
         msg = MIMEMultipart()
         msg['From'] = from_addr
         msg['To'] = to_addrs
         msg['Date'] = formatdate(localtime=True)
-        msg['Subject'] = u'{}质量报告'.format(cm.get_yesterday_date())
+        msg['Subject'] = u'{}-质量报告-{}'.format(project, cm.get_yesterday_date())
 
         body = ''
-        msg.attach(MIMEText(body, 'plain','utf-8'))
+        msg.attach(MIMEText(body, 'plain', 'utf-8'))
 
         with open(zip_path, "rb") as file:
             part = MIMEBase('application', 'octet-stream')
@@ -36,7 +38,6 @@ class EmailClient:
             encoders.encode_base64(part)
             part.add_header('Content-Disposition', 'attachment; filename="{}"'.format(os.path.basename(zip_path)))
             msg.attach(part)
-
 
         try:
             server = smtplib.SMTP_SSL(smtp_server, smtp_port)
